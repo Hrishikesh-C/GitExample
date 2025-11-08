60c431e8b0d35eb87a4228a0d547a58ad6ec3d68
Hrishikesh-C <hrishisham9608@gmail.com>
2025-11-08 08:24:37 +0530

    Second Commit

---
+++ src/com/git/example/Demo1.java
@@ -0,0 +1,14 @@
+package com.git.example;
+
+import java.util.Scanner;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int c=a+b;
+		System.out.println("sum of the number: "+c);
+	}
+}
